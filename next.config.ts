e782b708066e894e59436b016cc6133ed56a874c
Manish Singh Chauhan <manish.icst@gmail.com>
2026-02-05 01:02:16 +0530

    Fix PDF Parse Build: Ignore Canvas in Webpack

--- next.config.ts
+++ next.config.ts
@@ -62,6 +62,10 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
 };
 
 export default nextConfig;
